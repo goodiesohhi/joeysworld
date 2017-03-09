a60d6191485bfb20f050ed2227095b60c174ef1f
goodiesohhi <goodiesohhi@hotmail.com>
2017-03-08 21:43:17 -0500

    joey

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Joey's World" });
 });
+
+router.get('/disndat', function(req, res, next) {
+  res.render('translator', { title: "Brooklyn Rage" });
+});
+
 
 module.exports = router;
